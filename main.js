d484ac451a8242330173e1f4e357474786fa06cb
Drubajyoti Debnath <60551363+unknown009d@users.noreply.github.com>
2022-12-03 01:18:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let clearReset = () => {
 
 const ltForm = document.getElementById("tkForm");
 
-const elements = [2001,2002,2003,2004,2005,2006,2006,2007,2008,2009,2010];
+const elements = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010];
 
 let showTickets = () => {
     elements.forEach((data,count) => {
